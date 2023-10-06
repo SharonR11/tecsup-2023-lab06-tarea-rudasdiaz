63aa1e61c4550b8cb92cc8daa6fa52f4a2d9ffff
SharonR11 <sharon.rudas@tecsup.edu.pe>
2023-10-05 23:24:14 -0500

    update

--- source/index.js
+++ source/index.js
@@ -9,21 +9,5 @@ app.get('/', async(req, res, next) =>{
     //res.send(__dirname+ 'public/')
     })
 
-function validarFormulario() {
-    const nombre = document.querySelector('#nombre').value;
-    const email = document.querySelector('#email').value;
-    const password = document.querySelector('#password').value;
-    const fechaNacimiento = document.querySelector('#fechaNacimiento').value;
-
-    if (!nombre) return alert('El campo Nombre es obligatorio.');
-    if (email && !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) 
-    return alert('El correo electrónico no tiene un formato válido.');
-    if (!password) return alert('El campo Contraseña es obligatorio.');
-    if (!fechaNacimiento) return alert('El campo Fecha de Nacimiento es obligatorio.');
-
-    alert('Formulario enviado correctamente.');
-    return true;
-}
-
 app.listen(port)
 console.log(`Server on port ${port}`)
